27f280ae6bfdfc9c7f237cc98684c332f51c07c1
prankush <prankushrana2296@gmail.com>
2023-03-14 20:56:42 +0530

    final commit

--- main.js
+++ main.js
@@ -5,3 +5,11 @@ function myPort(){
 // function myEd(){
 //     location.href = '#ed';
 // }
+// document.getElementById("res").addEventListener("click",()=>{
+//     window.open('', '_blank');
+// })
+
+function resumeDownload(){
+   
+    window.open("https://drive.google.com/file/d/1lzMXWPqoVCkEXWd-REdT5JWIs4f6Hwwo/view?usp=sharing","_blank");
+  }
